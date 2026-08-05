6fe772ec48164b00a52f9618bac55933fbd676c0
Andrie Ador <154397358+adorandres@users.noreply.github.com>
2026-08-06 01:06:19 +0800

    Refactor aside navigation structure and classes

--- analytics.php
+++ analytics.php
@@ -27,58 +27,49 @@ $customers = mysqli_query($conn, "SELECT customer_id, ownername FROM customers O
 <body>
     <div class="container">
 
-        <!-- aside -->
-        <aside>
-
+        <!--aside--->
+        <aside class="navigation">
             <div class="top">
-
                 <div class="logo">
                     <h2><span class="danger">Pawcketful</span></h2>
                 </div>
-                <div class="close" id="close_btn">
-                    <span class="material-symbols-outlined"> close </span>
-                </div>
+                <span class="material-symbols-outlined">pets</span>
             </div>
-
-
-
-            <!-- Sidebar -->
 
             <div class="sidebar">
                 <a href="index.php">
-                    <span class="material-symbols-outlined">dashboard </span>
+                    <span class="material-symbols-outlined">dashboard</span>
                     <h3>Dashboard</h3>
                 </a>
                 <a href="appointments.php">
-                    <span class="material-symbols-outlined">calendar_add_on </span>
+                    <span class="material-symbols-outlined">calendar_add_on</span>
                     <h3>Appointments</h3>
                 </a>
                 <a href="customer.php">
-                    <span class="material-symbols-outlined">pets </span>
+                    <span class="material-symbols-outlined">pets</span>
                     <h3>Customers</h3>
                 </a>
-                <a href="analytics.php">
-                    <span class="material-symbols-outlined">analytics </span>
+                <a href="analytics.php" class="active">
+                    <span class="material-symbols-outlined">analytics</span>
                     <h3>Analytics</h3>
                 </a>
                 <a href="message.php">
-                    <span class="material-symbols-outlined">inbox </span>
+                    <span class="material-symbols-outlined">inbox</span>
                     <h3>Messages</h3>
                 </a>
                 <a href="report.php">
-                    <span class="material-symbols-outlined">medical_information </span>
+                    <span class="material-symbols-outlined">medical_information</span>
                     <h3>Reports</h3>
                 </a>
                 <a href="setting.php">
-                    <span class="material-symbols-outlined">settings </span>
+                    <span class="material-symbols-outlined">settings</span>
                     <h3>Settings</h3>
                 </a>
                 <a href="logout.php">
-                    <span class="material-symbols-outlined">logout </span>
+                    <span class="material-symbols-outlined">logout</span>
                     <h3>Logout</h3>
                 </a>
             </div>
-
         </aside>
 
 
